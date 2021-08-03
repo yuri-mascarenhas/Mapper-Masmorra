e6dd680622f07966bba1c0f8bf18ea339a62cd97
Yuri Mascarenhas <mascarenhasyuri@hotmail.com>
2021-08-03 19:28:44 -0300

    Implementado mais opções de tiles no menu

--- Menu.py
+++ Menu.py
@@ -17,7 +17,17 @@ class Menu(object):
                          Sprite("assets/buttons/save.png")]
         self._canvas = [Sprite("assets/tiles/chao0.png"),
                         Sprite("assets/tiles/paredeCID.png"),
-                        Sprite("assets/tiles/paredeCIE.png")]
+                        Sprite("assets/tiles/paredeCIE.png"),
+                        Sprite("assets/tiles/paredeCSD.png"),
+                        Sprite("assets/tiles/paredeCSE.png"),
+                        Sprite("assets/tiles/paredeMI.png"),
+                        Sprite("assets/tiles/paredeMS.png"),
+                        Sprite("assets/tiles/paredeTriID.png"),
+                        Sprite("assets/tiles/paredeTriIE.png"),
+                        Sprite("assets/tiles/paredeTriSD.png"),
+                        Sprite("assets/tiles/paredeTriSE.png"),
+                        Sprite("assets/tiles/paredeE.png"),
+                        Sprite("assets/tiles/paredeD.png"),]
         self.position_elements()
    
     """Define onde cada opção de botão e tile para uso do usuário ficará na tela"""
@@ -25,9 +35,11 @@ class Menu(object):
         for i in range(len(self._buttons)):
             self._buttons[i].x = 736
             self._buttons[i].y = 616 + (i * 36)
-        for i in range(len(self._canvas)):
-            self._canvas[i].x = 16
-            self._canvas[i].y = 616 + (i * 64)
+        for i in range(0, len(self._canvas), 3):
+            for j in range(3):
+                if(i + j < len(self._canvas)):
+                    self._canvas[i + j].x = 16 + (i * 32)
+                    self._canvas[i + j].y = 616 + (j * 64)
     
     """Retorna a lista que contém o canvas"""
     def get_canvas(self):
